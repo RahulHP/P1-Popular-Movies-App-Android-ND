eebc98661b9cdf40763f83514733eba4569cffdb
RahulHP <rahul.hphatak@gmail.com>
2015-12-20 20:09:39 +0530

    Setting xml added

--- app/src/main/java/com/wordpress/rahulhp/freshmovies/MainActivity.java
+++ app/src/main/java/com/wordpress/rahulhp/freshmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wordpress.rahulhp.freshmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -37,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
